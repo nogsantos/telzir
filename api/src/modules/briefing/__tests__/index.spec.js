90a3c1c3cb943755be388994bd386d3870469315
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-04 09:01:37 -0200

    test(api-tests): Test description
    Term corrections on test description

--- api/src/modules/briefing/__tests__/index.spec.js
+++ api/src/modules/briefing/__tests__/index.spec.js
@@ -3,36 +3,36 @@ const Briefing = require('../');
  * Must test briefing class
  */
 describe('Briefing', () => {
-	it('It reject to return a list when has no connection', () => {
+	it('Should reject to return a list when has no connection', () => {
 		const briefing = new Briefing({});
 		briefing.list().catch(e => {
 			expect(e).toBe('No database connection');
 		});
 	});
 
-	it('It test if is valid when all required values are valid', () => {
+	it('Should test if is valid when all required values are valid', () => {
 		const briefing = new Briefing({ ref_id: 30 });
 		expect(briefing.isValid()).toBeTruthy();
 	});
 
-	it('It test if is not valid when all of required values are invalid', () => {
+	it('Should test if is not valid when all of required values are invalid', () => {
 		const briefing = new Briefing({});
 		expect(briefing.isValid()).toBeFalsy();
 	});
 
-	it('It returns false when has no database connection', () => {
+	it('Should returns false when has no database connection', () => {
 		const briefing = new Briefing({});
 		expect(briefing.hasConnection()).toBeFalsy();
 	});
 
-	it('It reject to get value by reference when no refereces is invalid', () => {
+	it('Should reject to get value by reference when no refereces is invalid', () => {
 		const briefing = new Briefing({});
 		briefing.getByReference().catch(e => {
 			expect(e).toBe('Field(s) ref_id values can not be empty');
 		});
 	});
 
-	it('It reject to populate database when has no connection', () => {
+	it('Should reject to populate database when has no connection', () => {
 		const briefing = new Briefing({});
 		briefing.setup().catch(e => {
 			expect(e).toBe('No database connection');
